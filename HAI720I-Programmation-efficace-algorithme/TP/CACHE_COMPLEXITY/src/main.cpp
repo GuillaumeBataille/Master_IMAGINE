9ad05658966ae554ce2a45b90a56b550e30783fe
mylgasia <guillaumebataille9@gmail.com>
2022-10-27 15:24:45 +0200

    Test pea

--- HAI720I-Programmation-efficace-algorithme/TP/CACHE_COMPLEXITY/src/main.cpp
+++ HAI720I-Programmation-efficace-algorithme/TP/CACHE_COMPLEXITY/src/main.cpp
@@ -2,43 +2,37 @@
 #include <immintrin.h>
 #include <random>
 #include <iostream>
-#include <cstdlib> // Random
+#include <cstdlib>   // Random
 #include <algorithm> // std::mins
 #include "Eval-perf.h"
 #include "exo1.h"
 
-
-
-int main(){
-
+int main()
+{
 
     int N;
     size_t n;
-    std::cout<<"Donnez la valeur de n : "<<std::endl;
+    std::cout << "Donnez la valeur de n : " << std::endl;
     std::cin >> n;
-    // Valeurs initiales 
+    // Valeurs initiales
 
-    std::vector<float> A,v,temp,y;
+    std::vector<float> A, v, temp, y;
     size_t k;
-    A.resize(n*n,2.);
-    v.resize(n,5);
-    temp.resize(n,0.);
-    y.resize(n,0.);
-    
+    A.resize(n * n, 2.);
+    v.resize(n, 5);
+    temp.resize(n, 0.);
+    y.resize(n, 0.);
 
     EvalPerf PE;
 
-
-
     PE.start();
 
-    mat2vec_transp(n,A,v,y,temp,k);
+    mat2vec_transp(n, A, v, y, temp, k);
     //mat2vec(n,A,v,y,temp,k);
-
 
     PE.stop();
 
-    N = 4*n*n - 2*n;
+    N = 4 * n * n - 2 * n;
     std::cout << "nb cycles: " << PE.nb_cycle() << std::endl;
     std::cout << "nb secondes: " << PE.second() << std::endl;
     std::cout << "nb millisecondes: " << PE.millisecond() << std::endl;
